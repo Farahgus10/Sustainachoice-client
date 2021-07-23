b2ad24897f0335939d6b2a9f96c208c17f2474de
Farah Gustafson <farah.gus@hotmail.com>
2021-07-23 13:02:38 -0400

    single location page

--- src/Components/Location/location.js
+++ src/Components/Location/location.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import Nav from '../Nav/Nav'
 
 export default class Location extends React.Component {
     state = {
         
     }
 
+    renderLocationTitle = () => {
+        return this.props.location.map(result => 
+            <div>
+                <h1>{result.location_name}</h1>
+                <p>{result.info}</p>
+            </div>
+            
+        )
+    }
+
     render() {
         console.log(this.props.location)
         return(
-            <div>
+            <div className="location_page">
+                <Nav />
+                {this.renderLocationTitle()}
+                <div>
 
+                </div>
             </div>
         )
     }
